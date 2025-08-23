1a0bcb216b8576825eec4133ca90cd011d0cf310
Jash Kapoor <jashmkapoor@gmail.com>
2025-08-23 20:26:56 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import {ENV} from "./config/env.js";
+
+const app = express();
+
+app.get("/", (req, res)=> {
+    res.send("Hello World! 123");
+});
+console.log("mongo_uri:", ENV.MONGO_URI);
+
+app.listen(ENV.PORT, ()=> console.log("Server running on port :", ENV.PORT));
