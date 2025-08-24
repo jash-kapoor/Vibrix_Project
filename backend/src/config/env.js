160d992005aa54495c09834ea6938d6e281a41a9
Jash Kapoor <jashmkapoor@gmail.com>
2025-08-24 15:37:37 +0530

    db connected & env.js completed

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,4 +3,9 @@ export const ENV = {
     PORT: process.env.PORT || 5001,
     MONGO_URI: process.env.MONGO_URI,
     NODE_ENV: process.env.NODE_ENV,
+    CLERK_PUBLISHABLE_KEY: process.ENV.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.ENV.CLERK_SECRET_KEY,
+    SENTRY_DSN: process.ENV.SENTRY_DSN,
+    INNGEST_EVENT_KEY = process.ENV.INGGEST_EVENT_KEY,
+    INNGEST_SIGNIN_KEY = process.ENV.INGGEST_SIGNIN_KEY,
 };
