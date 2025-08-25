7996d35a45ab3c9b6f655040116f530237dcb97b
Jash Kapoor <jashmkapoor@gmail.com>
2025-08-26 00:02:00 +0530

    twice rollback

--- backend/src/controllers/auth.controller.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { clerkAuthCallback } from "../controllers/auth.controller.js";
-
-const router = express.Router();
-
-router.post("/auth/callback", clerkAuthCallback);
-
-export default router;
