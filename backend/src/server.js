97f46c2233bd1a087de4ab95770c5513ad34cc26
Jash Kapoor <jashmkapoor@gmail.com>
2025-08-25 11:53:19 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,35 @@
 import express from 'express';
 import {ENV} from "./config/env.js";
 import { connectDB } from './config/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./config/inngest.js"
 
 const app = express();
+
+app.use(express.json());
+app.use(clerkMiddleware()); //req.auth will be available in the request obj by writing
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res)=> {
     res.send("Hello World! 123");
 });
 console.log("mongo_uri:", ENV.MONGO_URI);
 
-app.listen(ENV.PORT, ()=> {
-    console.log("Server running on port :", ENV.PORT);
-    connectDB();
-});
+const startServer = async () => {
+try {
+    await connectDB();
+    if (ENV.NODE_ENV != "production") {
+        app. listen (ENV. PORT, () => {
+        console. log ("Server started on port:", ENV. PORT) ;
+        });
+    }
+  } catch (error) {
+    console.error ("Error starting server:", error);
+    process.exit (1); // Exit the process with a failure code
+    }
+};
+
+startServer();
+export default app;
